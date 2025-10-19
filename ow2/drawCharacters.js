7def332a36abed8bea98e0f19982e9d82bf81985
YOSHIHIRO11\yoshihiro <yoshihiro.sirius@gmail.com>
2025-10-19 21:02:00 +0900

    JSファイル分割

---
+++ ow2/drawCharacters.js
@@ -0,0 +1,84 @@
+function drawCharacters(ctx) {
+  const characterPaths = {
+    "タンク": "assets/character/tank",
+    "ダメージ": "assets/character/damage",
+    "サポート": "assets/character/support"
+  };
+
+  const characters = {
+    "タンク": [
+      "D.Va.png", "ウィンストン.png", "オリーサ.png", "ザリア.png", "シグマ.png",
+      "ジャンカー・クイーン.png", "ドゥームフィスト.png", "ハザード.png", "マウガ.png",
+      "ラインハルト.png", "ラマットラ.png", "レッキング・ボール.png", "ロードホッグ.png"
+    ],
+    "ダメージ": [
+      "リーパー.png", "アッシュ.png", "ウィドウメイカー.png", "エコー.png", "キャスディ.png",
+      "ゲンジ.png", "ジャンクラット.png", "シンメトラ.png", "ソジョーン.png", "ソルジャー76.png",
+      "ソンブラ.png", "トールビョーン.png", "トレーサー.png", "バスティオン.png", "ハンゾー.png",
+      "ファラ.png", "フレイヤ.png", "ベンチャー.png", "メイ.png"
+    ],
+    "サポート": [
+      "ルシオ.png", "アナ.png", "イラリー.png", "ウーヤン.png", "キリコ.png",
+      "ジュノ.png", "ゼニヤッタ.png", "バティスト.png", "ブリギッテ.png", "マーシー.png",
+      "モイラ.png", "ライフウィーバー.png"
+    ]
+  };
+
+  const roleLabels = {
+    "タンク": "Tank",
+    "ダメージ": "Damage",
+    "サポート": "Support"
+  };
+
+  const rolesForIcons = ["タンク", "ダメージ", "サポート"];
+  const iconsPerRow = 8;
+  const size = 60;
+  const padding = 10;
+  const startY = 450;
+
+  let currentY = startY;
+
+  rolesForIcons.forEach((role) => {
+    ctx.fillStyle = "#ccc";
+    ctx.font = "bold 28px sans-serif";
+    ctx.textAlign = "left";
+    ctx.fillText(roleLabels[role], 80, currentY - 20);
+
+    const charList = characters[role];
+    charList.forEach((file, i) => {
+      const img = new Image();
+      img.src = `${characterPaths[role]}/${file}`;
+
+      const x = 80 + (i % iconsPerRow) * (size + padding);
+      const y = currentY + Math.floor(i / iconsPerRow) * (size + 8);
+
+      // イメージ読み込み後に確実に描画
+      img.onload = (() => {
+        const drawX = x;
+        const drawY = y;
+        return () => {
+          ctx.drawImage(img, drawX, drawY, size, size);
+
+          // --- 状態（使用キャラ／未使用キャラ）描画 ---
+          const status = i % 5;
+          if (status === 4) {
+            ctx.fillStyle = "rgba(0,0,0,0.7)";
+            ctx.fillRect(drawX, drawY, size, size);
+          } else if (status === 0) {
+            ctx.fillStyle = "#ff6688";
+            ctx.font = "bold 26px sans-serif";
+            ctx.textAlign = "center";
+            ctx.textBaseline = "middle";
+            ctx.fillText("♥", drawX + size / 2 + 14, drawY + size / 2 - 14);
+            ctx.textAlign = "left";
+            ctx.textBaseline = "alphabetic";
+          }
+        };
+      })();
+    });
+
+    // === 次のロールの開始位置を動的にずらす ===
+    const rowCount = Math.ceil(charList.length / iconsPerRow);
+    currentY += rowCount * (size + 10) + 50;
+  });
+}
---
+++ ow2/drawHeader.js
@@ -0,0 +1,18 @@
+function drawHeader(ctx) {
+  // ====== プレイヤー情報 ======
+  ctx.fillStyle = "#fff";
+  ctx.font = "bold 36px sans-serif";
+  ctx.fillText("よっしー", 40, 80);
+
+  ctx.font = "24px sans-serif";
+  ctx.fillStyle = "#ccc";
+  ctx.fillText("Battletag: yoshihiro#11132", 40, 120);
+
+  // 区切り線
+  ctx.strokeStyle = "#666";
+  ctx.lineWidth = 2;
+  ctx.beginPath();
+  ctx.moveTo(40, 150);
+  ctx.lineTo(680, 150);
+  ctx.stroke();
+}
